4361c9181f37c305ea059dc288f13d83c6536d15
SantiagoReyVarela <99035265+SantiagoReyVarela@users.noreply.github.com>
2023-12-06 18:13:27 +0100

    Update  Scene1.js

--- Proyecto/Programacion/Escenas/Scene1.js
+++ Proyecto/Programacion/Escenas/Scene1.js
@@ -4,9 +4,18 @@ class Scene1 extends Phaser.Scene{
         super({key: 'Scene1', active:false})
     }
 
-    preload(){}
-  
-    create (){}
+    preload()
+    {
+        this.load.image('Jaulas', 'Proyecto/Arte/Bocetos/niveles prototipos/EscenarioJaulaPixelArt.png');
+        console.log("Se ha llegado a la Escena 1")
+        this.load.image('Kamaron', 'Arte/Bocetos/kamaron.png');
+    }
+    create ()
+    {
+        console.log("imagen del nivel 1 (jaulas)");
+        this.add.image(0, 0, 'Jaulas').setOrigin(0,0).setScale(0.2);
+        this.add.image(10, 10, 'Kamaron').setOrigin(0,0).setScale(0.2);
+    }
 
     update (){}
 }
