ec7e2e163268fc146f760ae707f8319ba1ca9ba9
NatillasConNata <nataliamartinezclemente@hotmail.es>
2023-12-06 01:43:09 +0100

    Actualizado Main Scene para que cambie de escenas al pulsar botón
    Ya está la implementación del salto de escenas, solo falta meterle todos los botones de la escena MainScene y que se dirijan a las escenas que tienen que ir

--- Proyecto/Programacion/Escenas/MainScene.js
+++ Proyecto/Programacion/Escenas/MainScene.js
@@ -7,18 +7,57 @@ class MainScene extends Phaser.Scene{
     preload()
     {
         this.load.image('Aviario', 'Arte/Bocetos/niveles prototipos/EscenarioAviarioPixelArtV1.jpg');
+        this.load.spritesheet('Buttons', 'Arte/Bocetos/UI/Buttons.png', { frameWidth: 24, frameHeight: 23 });
         console.log("he llegado hasta Main")
-        //this.load.image('Aviario', 'Arte/kamaron.png');
+        this.load.image('Kamaron', 'Arte/Bocetos/kamaron.png');
+
+        //barra de carga del juego
+        let barraCarga = this.add.graphics({
+            fillStyle: {
+                color:  0xFFFFFF
+
+            }
+        })
+        this.load.on("progress",(percent)=>{
+           // barraCarga.fillRect(0, this.game.renderer.height / 2, this.game.renderer.width * percent, 50); //error en this.game = undefined
+            console.log(percent);
+        }) 
+        this.load.on("complete",()=>{
+            console.log("DONEEEEEEE")
+        })
 
     }
     create ()
     {
-        console.log("imagen aviario");
+        //console.log("imagen aviario");
         this.add.image(0, 0, 'Aviario').setOrigin(0,0).setScale(0.2);
+        var startButton = this.add.image(500, 500, 'Buttons', 52).setScale(3).setInteractive();
+        var startButton2 = this.add.image(572, 500, 'Buttons', 53).setScale(3).setInteractive();
+
+
+    //pulsar botón cambia de escena
+    //START BUTTON
+    startButton.on('pointerdown', function () {
+        this.scene.stop('MainScene');/**************** */
+        this.scene.start('Scene2', Scene2, true, { x: 400, y: 300 });
+    
+    },this);
+    startButton2.on('pointerdown', function () {
+        this.scene.stop('MainScene');/**************** */
+        this.scene.start('Scene2', Scene2, true, { x: 400, y: 300 });
+    
+    },this);
+    //CONFIG BUTTON
+
+    //CREDITS BUTTON
+
+    //EXIT BUTTON
+
     }
 
     update ()
     {
 
     }
+
 }
--- Proyecto/Programacion/Escenas/Scene2.js
+++ Proyecto/Programacion/Escenas/Scene2.js
@@ -7,14 +7,14 @@ class Scene2 extends Phaser.Scene{
     preload()
     {
         this.load.image('Aviario', 'Arte/Bocetos/niveles prototipos/EscenarioAviarioPixelArtV1.jpg');
-        console.log("he llegado hasta Main")
-        //this.load.image('Aviario', 'Arte/kamaron.png');
-
+        console.log("he llegado hasta Scene2")
+        this.load.image('Kamaron', 'Arte/Bocetos/kamaron.png');
     }
     create ()
     {
         console.log("imagen aviario");
         this.add.image(0, 0, 'Aviario').setOrigin(0,0).setScale(0.2);
+        this.add.image(10, 10, 'Kamaron').setOrigin(0,0).setScale(0.2);
     }
 
     update ()
